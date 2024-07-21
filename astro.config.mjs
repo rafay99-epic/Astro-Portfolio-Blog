61081d7ea59b191a9b3a4bc715c03acb80488c48
rafay99-epic <99marafay@gmail.com>
2024-07-21 14:11:04 +0500

    Website Structure & Into Complete

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import lottie from "astro-integration-lottie";
+import pagefind from "astro-pagefind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  build: {
+    format: "file",
+  },
+  integrations: [mdx(), sitemap(), lottie(), pagefind()],
+});
