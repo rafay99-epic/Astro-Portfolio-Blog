ec6f5f873a00b19411ff076ec8b6a2fe0143cf41
Abdul Rafay <82662797+rafay99-epic@users.noreply.github.com>
2024-07-26 21:10:29 +0500

    New Blog Post & About me (#7)
    * ReadMe File Update
    
    * Blog Post, Code Clean, Comments Section
    
    * more test
    
    * Github Action for Main branch
    
    * new Blog & About me improvement
    
    * removing .astro file
    
    * Adding View Transation
    
    * removing thumbnail

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -157,6 +157,15 @@ declare module 'astro:content' {
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".md"] };
+
+"flutter-mobile-web-desktop-experience.md": {
+	id: "flutter-mobile-web-desktop-experience.md";
+  slug: "flutter-mobile-web-desktop-experience";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+
 };
 
 	};
