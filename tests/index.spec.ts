ee12993754c3a0c462ce9dbe91cfaf1b57e954dc
Abdul Rafay <82662797+rafay99-epic@users.noreply.github.com>
2024-07-31 23:47:28 +0500

    Development: New UI Changes & Test Update (#14)
    * ReadMe File Update
    
    * Blog Post, Code Clean, Comments Section
    
    * more test
    
    * Github Action for Main branch
    
    * new Blog & About me improvement
    
    * removing .astro file
    
    * Adding View Transation
    
    * removing thumbnail
    
    * Image optimzation (#9)
    
    * Image Optimzation vis image Compoent
    
    * Project Cards Image Optimzation
    
    * Removing .astro folder
    
    * Image optimization (#10)
    
    * Image Optimzation vis image Compoent
    
    * Project Cards Image Optimzation
    
    * Removing .astro folder
    
    * Updating Playwright Test
    
    * Update .gitignore
    
    * Updating  test for about me
    
    * New UI (#13)
    
    * Hire Me button
    
    * Home Page, Layout Improvement & Animation Change
    
    * About, Index & Header Animation & Optization
    
    * Playwright Test Update
    
    * Playwright Config Update

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,18 +1,44 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Home Page", () => {
-  test("should load the home page and check animated text", async ({
-    page,
-  }) => {
-    // Navigate to the home page
+  test.beforeEach(async ({ page }) => {
     await page.goto("/");
-    const animatedText = await page.locator(".animated-text");
-    await expect(animatedText).toHaveCount(3);
+  });
 
-    await expect(animatedText.nth(0)).toHaveText("Hi There!");
-    await expect(animatedText.nth(1)).toHaveText("I'm Abdul Rafay");
-    await expect(animatedText.nth(2)).toHaveText(
+  test("should display the header", async ({ page }) => {
+    const header = page.locator("header");
+    await expect(header).toBeVisible();
+  });
+
+  test("should display the greeting text", async ({ page }) => {
+    const greeting = page.locator("h1.animated-text");
+    await expect(greeting).toHaveText("Hi There!");
+  });
+
+  test("should display the author name", async ({ page }) => {
+    const authorName = page.locator("h2.animated-text");
+    await expect(authorName).toHaveText("I'm Abdul Rafay");
+  });
+
+  test("should display the description", async ({ page }) => {
+    const description = page.locator("p.description");
+    await expect(description).toHaveText(
       "Software Engineer | Full Stack & Flutter Developer"
     );
+  });
+
+  test("should display the social links", async ({ page }) => {
+    const socialLinks = page.locator(".social-links-container");
+    await expect(socialLinks).toBeVisible();
+  });
+
+  test("should display the connect button", async ({ page }) => {
+    const connectButton = page.locator("button.connect-button");
+    await expect(connectButton).toBeVisible();
+  });
+
+  test("should display the Lottie animation", async ({ page }) => {
+    const lottieAnimation = page.locator(".lottie-animation-container");
+    await expect(lottieAnimation).toBeVisible();
   });
 });
