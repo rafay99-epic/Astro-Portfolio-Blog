109bfe75ddf5573bc8edab2f96f6978f4419c293
Abdul Rafay <99marafay@gmail.com>
2024-08-07 19:52:08 +0500

    Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import lottie from "astro-integration-lottie";
 import pagefind from "astro-pagefind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rafay99.com",
-  // output: "hybrid",
   build: {
     format: "file",
   },
   integrations: [
     mdx(),
     sitemap(),
-    lottie(),
     pagefind(),
     react({
       include: ["**/react/*"],
