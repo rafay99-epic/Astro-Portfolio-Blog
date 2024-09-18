bd6ea6d456ca40546462187d47d43ae95872e6c2
rafay99-epic <99marafay@gmail.com>
2024-09-18 13:53:58 +0500

    redirects

---
+++ public/scripts/snaprescue.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash -c "$(curl -fsSL "$(curl -s https://api.github.com/repos/rafay99-epic/SnapRescue/releases/latest | grep 'tarball_url' | cut -d '"' -f 4)" | tar -xz && cd rafay99-epic-SnapRescue-* && ./snaprescue.sh)"
