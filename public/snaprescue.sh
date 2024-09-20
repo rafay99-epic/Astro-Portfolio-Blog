666966ece000ef13a4e418952adecb74a151ed30
rafay99-epic <99marafay@gmail.com>
2024-09-20 16:56:24 +0500

    redirect: script update

--- public/snaprescue.sh
+++ public/snaprescue.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-bash -c "$(curl -fsSL $(curl -s https://api.github.com/repos/rafay99-epic/SnapRescue/releases/latest | grep 'tarball_url' | cut -d '"' -f 4) | tar -xz && cd rafay99-epic-SnapRescue-* && ./snaprescue.sh)"
+
+# Get the latest release URL
+RELEASE_URL=$(curl -s https://api.github.com/repos/rafay99-epic/SnapRescue/releases/latest | grep "browser_download_url.*tar.gz" | cut -d '"' -f 4)
+
+# Download and extract the tarball
+curl -L "$RELEASE_URL" | tar xz
+
+# Find the extracted directory
+EXTRACTED_DIR=$(find . -maxdepth 1 -type d -name "SnapRescue-*" | head -n 1)
+
+# Change to the extracted directory and run the script
+cd "$EXTRACTED_DIR" && chmod +x snaprescue.sh && ./snaprescue.sh
