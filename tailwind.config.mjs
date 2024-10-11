4772fec1c06922ab65954ca704c89f4f8d8d4163
Abdul Rafay <82662797+rafay99-epic@users.noreply.github.com>
2024-10-11 14:35:16 +0500

    ✨ Version 3.0: New UI and Some More Changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,16 +4,16 @@ export default {
   theme: {
     extend: {
       fontFamily: { sans: ["Atkinson", "sans-serif"] },
-      colors: {
-        accent: "var(--accent)",
-        "accent-dark": "var(--accent-dark)",
-        black: "rgb(var(--black))",
-        gray: "rgb(var(--gray))",
-        "gray-light": "rgb(var(--gray-light))",
-        "text-light": "var(--text-light)",
-        "gray-dark": "rgb(var(--gray-dark))",
-        "gray-gradient": "var(--gray-gradient)",
-      },
+      // colors: {
+      //   accent: "var(--accent)",
+      //   "accent-dark": "var(--accent-dark)",
+      //   black: "rgb(var(--black))",
+      //   gray: "rgb(var(--gray))",
+      //   "gray-light": "rgb(var(--gray-light))",
+      //   "text-light": "var(--text-light)",
+      //   "gray-dark": "rgb(var(--gray-dark))",
+      //   "gray-gradient": "var(--gray-gradient)",
+      // },
       boxShadow: {
         custom:
           "0 2px 6px rgba(76, 80, 106, 0.25), 0 8px 24px rgba(76, 80, 106, 0.33)",
