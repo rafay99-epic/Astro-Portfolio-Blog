fff101afe6b57de34f1bbd1e134eeeb3f7c74ad2
Abdul Rafay <99marafay@gmail.com>
2024-10-13 00:51:12 +0500

    Android & IOS Application

---
+++ android/app/src/main/java/com/99lineofcode/MainActivity.java
@@ -0,0 +1,5 @@
+package com.99lineofcode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.99lineofcode',
+  appName: 'rafayblog',
+  webDir: '.vercel/output/static/'
+};
+
+export default config;
