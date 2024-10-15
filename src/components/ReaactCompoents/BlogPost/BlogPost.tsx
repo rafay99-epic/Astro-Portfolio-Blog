dcc599d4c40fc9fcf123fe70b9d8ad22cafd7e88
rafay99-epic <99marafay@gmail.com>
2024-10-16 02:21:33 +0500

    BlogPost: Adding Check no Blog Post

--- src/components/ReaactCompoents/BlogPost/BlogPost.tsx
+++ src/components/ReaactCompoents/BlogPost/BlogPost.tsx
@@ -1,5 +1,5 @@
-import BlogSectionLogic from "./BlogPostLogic"; // Adjust the import path based on your project structure
-import BlogSectionUI from "./BlogPostUI"; // Adjust the import path based on your project structure
+import BlogSectionLogic from "./BlogPostLogic";
+import BlogSectionUI from "./BlogPostUI";
 
 interface Post {
   id: string;
@@ -23,6 +23,26 @@ interface BlogSectionProps {
 }
 
 const BlogSection: React.FC<BlogSectionProps> = ({ posts }) => {
+  if (posts.length === 0) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center mt-[-10vh] space-y-6 p-8 ">
+        <h2 className="text-3xl font-bold text-[#7aa2f7] tracking-wide">
+          No Blogs Available Yet
+        </h2>
+        <p className="text-lg text-gray-400">
+          Stay tuned! New content is coming soon. In the meantime, check out the
+          Newsletter or subscribe for updates.
+        </p>
+        <a
+          href="/"
+          className="inline-block px-6 py-3 bg-[#7aa2f7] text-white font-medium rounded-lg shadow-md hover:bg-[#5f90e8] transition duration-300"
+        >
+          Go Back to Home
+        </a>
+      </div>
+    );
+  }
+
   return (
     <BlogSectionLogic posts={posts}>
       {(currentPosts, totalPages, currentPage, handlePageChange) => (
