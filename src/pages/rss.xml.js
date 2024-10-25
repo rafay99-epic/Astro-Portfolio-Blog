87141927e8b2e500b19dc2f16e8afc70e9aac657
Abdul Rafay <99marafay@gmail.com>
2024-10-25 11:44:14 +0500

    updating rss field

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,27 +1,6 @@
-// import rss from "@astrojs/rss";
-// import { getCollection } from "astro:content";
-// import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
-
-// export async function GET(context) {
-//   const posts = await getCollection("blog");
-
-//   return rss({
-//     title: SITE_TITLE,
-//     description: SITE_DESCRIPTION,
-//     site: context.site,
-//     items: posts.map((post) => ({
-//       title: post.data.title,
-//       description: post.data.description,
-//       pubDate: post.data.pubDate,
-//       link: `/blog/${post.slug}/`,
-//       author: post.data.authorName,
-//     })),
-//   });
-// }
-
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import authorConfig from "../config/info";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -32,13 +11,13 @@ export async function GET(context) {
       description: "Stay updated by subscribing to our newsletter.",
       pubDate: new Date(),
       link: "/newsletter/",
-      author: "Website Team",
+      author: authorConfig.name,
     },
   ];
 
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: authorConfig.SiteName,
+    description: authorConfig.SiteDescription,
     site: context.site,
     items: [
       ...posts.map((post) => ({
