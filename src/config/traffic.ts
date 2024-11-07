2442bf061de043e432b304b512defc5ab674b939
rafay99-epic <99marafay@gmail.com>
2024-11-07 20:02:43 +0500

    Bug Fixed: Feature Post Ranking Fixed

---
+++ src/config/traffic.ts
@@ -0,0 +1,16 @@
+// src/data/popularBlogs.ts
+export const popularBlogs = [
+  { Page: "/blog/flutter_fork_flock", Visitors: 74, Total: 82 },
+  { Page: "/blog", Visitors: 25, Total: 35 },
+  { Page: "/blog/ai-coding-machine-learning", Visitors: 12, Total: 16 },
+  { Page: "/blog/web-assembly-in-flutter", Visitors: 7, Total: 8 },
+  { Page: "/blog/search-on-static-site", Visitors: 5, Total: 7 },
+  { Page: "/blog/diy-ai-chatbot", Visitors: 3, Total: 4 },
+  { Page: "/blog/deploy-your-application-the-journey", Visitors: 1, Total: 5 },
+  {
+    Page: "/blog/exploring-react-a-full-stack-developers-perspective",
+    Visitors: 1,
+    Total: 1,
+  },
+  { Page: "/blog/text-search-application-python", Visitors: 1, Total: 1 },
+];
