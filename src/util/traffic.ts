79fe00757a24cc2def2ff147d70d5c313eba78f9
rafay99-epic <99marafay@gmail.com>
2024-11-18 23:12:20 +0500

    New Traffic

--- src/util/traffic.ts
+++ src/util/traffic.ts
@@ -1,15 +1,16 @@
-type PageAnalytics = {
-  Page: string;
-  Visitors: number;
-  Total: number;
-};
+import type { PageAnalytics } from "@util/types";
 
 export const popularBlogs: PageAnalytics[] = [
-  { Page: "/", Visitors: 210, Total: 322 },
+  { Page: "/", Visitors: 217, Total: 332 },
   { Page: "/blog/flutter_fork_flock", Visitors: 74, Total: 82 },
-  { Page: "/blog/automate-life-in-linux", Visitors: 39, Total: 43 },
+  { Page: "/blog/automate-life-in-linux", Visitors: 40, Total: 44 },
   { Page: "/blog/search-on-static-site", Visitors: 35, Total: 44 },
-  { Page: "/blog", Visitors: 31, Total: 45 },
+  { Page: "/blog", Visitors: 33, Total: 47 },
+  {
+    Page: "/blog/there-another-new-ai-fork-of-vs-code",
+    Visitors: 26,
+    Total: 29,
+  },
   { Page: "/blog/embrace_ai_tools", Visitors: 22, Total: 29 },
   { Page: "/blog/ai-coding-machine-learning", Visitors: 13, Total: 17 },
   { Page: "/search", Visitors: 12, Total: 14 },
@@ -19,27 +20,4 @@ export const popularBlogs: PageAnalytics[] = [
     Total: 13,
   },
   { Page: "/project", Visitors: 10, Total: 16 },
-  { Page: "/about", Visitors: 9, Total: 9 },
-  { Page: "/contact-me", Visitors: 6, Total: 6 },
-  { Page: "/tag", Visitors: 6, Total: 10 },
-  { Page: "/newsletter-subscribe", Visitors: 5, Total: 5 },
-  { Page: "/privacy-policy", Visitors: 3, Total: 4 },
-  { Page: "/api/notion", Visitors: 1, Total: 1 },
-  { Page: "/blog-placeholder-1.jpg", Visitors: 1, Total: 1 },
-  { Page: "/blog/deploy-your-application-the-journey", Visitors: 1, Total: 5 },
-  { Page: "/blog/diy-ai-chatbot", Visitors: 1, Total: 1 },
-  {
-    Page: "/blog/exploring-react-a-full-stack-developers-perspective",
-    Visitors: 1,
-    Total: 1,
-  },
-  { Page: "/blog/mylovehaterelationshipwithcopilot", Visitors: 1, Total: 2 },
-  { Page: "/blog/no-code-tools", Visitors: 1, Total: 1 },
-  { Page: "/blog/text-search-application-python", Visitors: 1, Total: 1 },
-  { Page: "/contact", Visitors: 1, Total: 1 },
-  { Page: "/newsletter", Visitors: 1, Total: 1 },
-  { Page: "/project/future-insight", Visitors: 1, Total: 2 },
-  { Page: "/project/immune-defenders", Visitors: 1, Total: 1 },
-  { Page: "/project/shafiqlawchamber", Visitors: 1, Total: 1 },
-  { Page: "/tags", Visitors: 1, Total: 1 },
 ];
