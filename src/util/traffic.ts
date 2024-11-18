af6c1fa33678ca51d1451cc026216c0d59811de4
Abdul Rafay <82662797+rafay99-epic@users.noreply.github.com>
2024-11-18 14:38:06 +0500

    Update traffic.ts

--- src/util/traffic.ts
+++ src/util/traffic.ts
@@ -1,23 +1,29 @@
-// src/data/popularBlogs.ts
-export const popularBlogs = [
-  { Page: "/", Visitors: 198, Total: 298 },
+type PageAnalytics = {
+  Page: string;
+  Visitors: number;
+  Total: number;
+};
+
+export const popularBlogs: PageAnalytics[] = [
+  { Page: "/", Visitors: 210, Total: 322 },
   { Page: "/blog/flutter_fork_flock", Visitors: 74, Total: 82 },
-  { Page: "/blog/automate-life-in-linux", Visitors: 27, Total: 30 },
-  { Page: "/blog", Visitors: 26, Total: 37 },
-  { Page: "/blog/embrace_ai_tools", Visitors: 21, Total: 28 },
+  { Page: "/blog/automate-life-in-linux", Visitors: 39, Total: 43 },
+  { Page: "/blog/search-on-static-site", Visitors: 35, Total: 44 },
+  { Page: "/blog", Visitors: 31, Total: 45 },
+  { Page: "/blog/embrace_ai_tools", Visitors: 22, Total: 29 },
   { Page: "/blog/ai-coding-machine-learning", Visitors: 13, Total: 17 },
-  { Page: "/about", Visitors: 8, Total: 8 },
+  { Page: "/search", Visitors: 12, Total: 14 },
   {
     Page: "/blog/make-money-from-open-source-software",
-    Visitors: 8,
-    Total: 11,
+    Visitors: 10,
+    Total: 13,
   },
-  { Page: "/project", Visitors: 7, Total: 11 },
+  { Page: "/project", Visitors: 10, Total: 16 },
+  { Page: "/about", Visitors: 9, Total: 9 },
+  { Page: "/contact-me", Visitors: 6, Total: 6 },
   { Page: "/tag", Visitors: 6, Total: 10 },
-  { Page: "/blog/search-on-static-site", Visitors: 5, Total: 7 },
-  { Page: "/contact-me", Visitors: 5, Total: 5 },
-  { Page: "/newsletter-subscribe", Visitors: 3, Total: 3 },
-  { Page: "/search", Visitors: 3, Total: 5 },
+  { Page: "/newsletter-subscribe", Visitors: 5, Total: 5 },
+  { Page: "/privacy-policy", Visitors: 3, Total: 4 },
   { Page: "/api/notion", Visitors: 1, Total: 1 },
   { Page: "/blog-placeholder-1.jpg", Visitors: 1, Total: 1 },
   { Page: "/blog/deploy-your-application-the-journey", Visitors: 1, Total: 5 },
@@ -31,7 +37,9 @@ export const popularBlogs = [
   { Page: "/blog/no-code-tools", Visitors: 1, Total: 1 },
   { Page: "/blog/text-search-application-python", Visitors: 1, Total: 1 },
   { Page: "/contact", Visitors: 1, Total: 1 },
-  { Page: "/privacy-policy", Visitors: 1, Total: 1 },
+  { Page: "/newsletter", Visitors: 1, Total: 1 },
+  { Page: "/project/future-insight", Visitors: 1, Total: 2 },
+  { Page: "/project/immune-defenders", Visitors: 1, Total: 1 },
   { Page: "/project/shafiqlawchamber", Visitors: 1, Total: 1 },
   { Page: "/tags", Visitors: 1, Total: 1 },
 ];
