b73ace16efbb8bbb75c21ad9a29caf753a974665
Abdul Rafay <99marafay@gmail.com>
2025-01-09 00:51:39 +0500

    Video Embed Update & Enabling Blog Post

--- src/components/ReaactCompoents/VideoEmbed/VideoEmbed.tsx
+++ src/components/ReaactCompoents/VideoEmbed/VideoEmbed.tsx
@@ -1,30 +1,22 @@
-// components/VideoEmbed.jsx
-import React from "react";
-
 const VideoEmbed = ({ src, title, width = "100%", height = "400px" }) => {
   return (
     <div
+      className="relative overflow-hidden rounded-lg shadow-md bg-gray-dark"
       style={{
-        position: "relative",
         paddingBottom: "56.25%",
         height: 0,
-        overflow: "hidden",
       }}
     >
       <iframe
         src={src}
         title={title}
-        width={width}
-        height={height}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        className="absolute top-0 left-0 w-full h-full"
         style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
+          width: width,
+          height: height,
         }}
       />
     </div>
