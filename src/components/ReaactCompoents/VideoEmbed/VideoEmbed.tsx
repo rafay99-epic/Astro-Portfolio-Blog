19314500f1f405ff81acd5cd54bc787fa1a9317d
Abdul Rafay <99marafay@gmail.com>
2025-01-09 00:28:50 +0500

    Video Embed & Bug Fixes

---
+++ src/components/ReaactCompoents/VideoEmbed/VideoEmbed.tsx
@@ -0,0 +1,34 @@
+// components/VideoEmbed.jsx
+import React from "react";
+
+const VideoEmbed = ({ src, title, width = "100%", height = "400px" }) => {
+  return (
+    <div
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%",
+        height: 0,
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src={src}
+        title={title}
+        width={width}
+        height={height}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </div>
+  );
+};
+
+export default VideoEmbed;
