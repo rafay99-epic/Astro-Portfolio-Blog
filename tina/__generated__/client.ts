e541da38d153e3d221c02e6a5e2df6ff49f6253c
Abdul Rafay <99marafay@gmail.com>
2025-03-04 21:53:52 +0500

    TinaCMS && MS Notes API

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '8824be588169be37ca1b34e4e50b4ed5715f6040', queries });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries });
 export default client;
   
