991ffeef1ee223b2df124f316cb9a99669b9e82b
Abdul Rafay <99marafay@gmail.com>
2025-03-27 02:32:23 +0500

    new Blog & Code Optization

--- src/components/ReaactCompoents/VideoEmbed/VideoEmbed.tsx
+++ src/components/ReaactCompoents/VideoEmbed/VideoEmbed.tsx
@@ -1,24 +1,37 @@
-const VideoEmbed = ({ src, title, width = "100%", height = "400px" }) => {
+const VideoEmbed = ({
+  src,
+  title,
+  aspectRatio = "16:9",
+  marginBottom = "1em",
+  maxWidth = "800px",
+}) => {
+  const calculatePaddingBottom = () => {
+    const [width, height] = aspectRatio.split(":").map(Number);
+    return (height / width) * 100 + "%";
+  };
+
+  const paddingBottom = calculatePaddingBottom();
+
   return (
-    <div
-      className="relative overflow-hidden rounded-lg  bg-gray-dark"
-      style={{
-        paddingBottom: "56.25%",
-        height: 0,
-      }}
-    >
-      <iframe
-        src={src}
-        title={title}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        className="absolute top-0 left-0 w-full h-full"
+    <div className="flex justify-center" style={{ marginBottom: marginBottom }}>
+      <div
+        className="relative overflow-hidden rounded-lg bg-gray-dark"
         style={{
-          width: width,
-          height: height,
+          paddingBottom: paddingBottom,
+          height: 0,
+          width: "100%",
+          maxWidth: maxWidth,
         }}
-      />
+      >
+        <iframe
+          src={src}
+          title={title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </div>
     </div>
   );
 };
