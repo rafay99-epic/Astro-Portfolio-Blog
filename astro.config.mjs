3eb32b080cd50c20e596da0528ceccfc8c60ef1b
Abdul Rafay <99marafay@gmail.com>
2025-06-25 08:33:48 +0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -94,10 +94,6 @@ export default defineConfig({
     },
     imageService: true,
     devImageService: "sharp",
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920],
-      formats: ["image/webp", "image/avif"],
-    },
   }),
   vite: {
     build: {
