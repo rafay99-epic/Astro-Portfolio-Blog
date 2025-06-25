67402d90761323611812c30ea6876451c5e16148
Abdul Rafay <99marafay@gmail.com>
2025-06-25 09:12:58 +0500

    versel adpator

--- astro.config.mjs
+++ astro.config.mjs
@@ -133,10 +133,5 @@ export default defineConfig({
     },
     imageService: true,
     devImageService: "sharp",
-    imagesConfig: {
-      formats: ["avif", "webp"],
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-      minimumCacheTTL: 60,
-    },
   }),
 });
