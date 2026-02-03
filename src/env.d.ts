2cd09bc022f5a1ff37f0651e4a7884a2e5f47f3e
Abdul Rafay <99marafay@gmail.com>
2026-02-03 21:56:10 +0500

    removing-playground

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,11 +8,4 @@ declare module "*.json" {
 interface Window {
   fnames?: string[];
   ftypes?: string[];
-  loadPyodide?: (options: { indexURL: string }) => Promise<any>;
-}
-
-// react-native-web type declaration
-declare module "react-native-web" {
-  const ReactNative: any;
-  export = ReactNative;
 }
